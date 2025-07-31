839cfc068e9bf00063d8caf350a8c0ba4776bb1b
Andre Garcia <andgarcia20@outlook.com>
2025-07-31 13:09:06 -0500

    first commit

---
+++ src/data/ecommerce.js
@@ -0,0 +1,14 @@
+export default [
+  {
+    title: 'PSK Adventure',
+    description: 'Tienda de artículos de caza y aventura.',
+    image: '/images/psk.png',
+    link: 'https://pskadventure.com'
+  },
+  {
+    title: 'Createc STORE',
+    description: 'Productos tecnológicos al mejor precio.',
+    image: '/images/createc.png',
+    link: 'https://createcstore.com'
+  }
+];
